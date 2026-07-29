f66217c8e2ed3da11b6c1736c50ed15d523dd9c8
zmfkorina <zamfirkorina@yahoo.com>
2026-07-29 20:58:59 +0300

    Added update book and some CSS

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.list_books, name='home'),
-    path('create_book/', views.create_book, name='create_book'),
-    path('delete_book/<int:pk>', views.delete_book, name='delete_book'),
+    path("", views.list_books, name='home'),
+    path("create_book/", views.create_book, name='create_book'),
+    path("delete_book/<int:pk>/", views.delete_book, name='delete_book'),
+    path("update_book/<int:pk>/", views.update_book, name='update_book'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- books/views.py
+++ books/views.py
@@ -36,6 +36,25 @@ def create_book(request: HttpRequest):
         list1 = [10, 20, 30, 40]
         return render(request, "books/book_form.html", context={"form": form, "list1": list1})
 
+
+
+
+def update_book(request: HttpRequest, pk: int):
+    book = get_object_or_404(Book, pk=pk)
+    # book = Book.objects.get(pk=pk)
+
+    if request.method == "POST":
+        #detaliile cartii care au fost trimise de form folosing HTTP POST request se afla in request.POST, care e un dictionar
+        book_instance = BookForm(request.POST, instance=book)
+        if book_instance.is_valid():
+            #aici se modifica o carte in baza de date
+            book_instance.save()
+            return redirect("home")
+    else:
+        #in cazul asta requestul poate fi GET, PUT, PATCH, DELETE, ETC
+        form = BookForm(instance=book)
+        return render(request, "books/update_book_form.html", context={"form": form})
+
 def delete_book(request: HttpRequest, pk: int):
     book = get_object_or_404(Book, pk=pk)
 
