a6f843b83794c0e3200f2211868a659eb587da52
zmfkorina <zamfirkorina@yahoo.com>
2026-07-30 00:48:29 +0300

    Updated login views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+
+from accounts.forms import RegisterForm
+
 
 # Create your views here.
+
+def login_user(request: HttpRequest):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # now we login our user.
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {"form": form})
+
+def register_user(request: HttpRequest):
+    pass
+
+def logout_user(request: HttpRequest):
+    pass
--- book_manager/settings.py
+++ book_manager/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
